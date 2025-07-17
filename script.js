59888f35df701413fc579ad3e5acfbe99bb14675
Jeremy Calcarian <calcajer@seas.upenn.edu>
2025-07-17 17:48:05 -0500

    Music Sectoin addition

--- script.js
+++ script.js
@@ -170,8 +170,89 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
-
-
-
-
+  const houseTracks = [
+    '5veX2gRTIhuwxyelOqeeOM',
+    '4lq9lGXtJnAPLWVZav5Uyh',
+    '2klm9vgplHbcAq5VIHZCu4',
+    '3f5LJiAEHHZtW8bzF4qpNr',
+    '5HphzVqJsiVij9zqLF5d14',
+    '5F9rZa65ePNd3HZFkavre7',
+    '2CtAajnRp5uwRCpQck0ki8',
+    '3X7zc5bxxXSZeHELV0I8DE',
+    '0u5aRUEwlWhtO32VASKoLi',
+    '1YBvhB1094Q5Niw1GDSRY3',
+    '6jhAJgaS9OttFwP5Cn8WII',
+    '7u0fz3V6cYeYTX91DMmIvQ',
+    '0xaXwvcjq7aAKwMKe22Bw7',
+    '1aNUSKBe6UMyMk3pEu9ws7',
+    '4zRvloWWpWHVgtdete6b1A',
+    '23RoR84KodL5HWvUTneQ1w',
+    '59NraMJsLaMCVtwXTSia8i',
+    '39iL6MNqs9MIile4ohbx6K',
+    '0FBdJP7yzvq88bG1keGgt4',
+    '04gs2fDnnjT6995ruR1qbk',
+    '30uUMdzRVdYd9KuP9rJXxo',
+    '6q36Cqt2d3O5jqrQR9uXCp',
+    '6Uz2230ZgSmqQli5SMaIZY',
+    '70fplEUWEuEIaJ2peNpPxW',
+    '0aeYqWitH0mkLtzcpeheWk',
+    '6q6GR1UxIkyaVJuUNYtEjw',
+    '78nx0HDJIFD5xDq2L5420Z',
+    '1iKiLPkFnYhbCm5uvaDwjS',
+    '2y7UV3mw1igF35pj4b3xn7'
+  ];
+  
+  const btn     = document.getElementById('recommend-btn');
+  const rec     = document.getElementById('recommendation');
+  const moreMsg = document.getElementById('more-msg');
+  
+  let clickCount = 0;
+  let shuffled = [];  // store shuffled copy
+  let index = 0;
+  
+  // Shuffle helper
+  function shuffleArray(array) {
+    const copy = [...array];
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+    return copy;
+  }
+  
+  // Initialize shuffle
+  function resetShuffle() {
+    shuffled = shuffleArray(houseTracks);
+    index = 0;
+  }
+  
+  resetShuffle();
+  
+  btn.addEventListener('click', () => {
+    clickCount++;
+  
+    if (clickCount > 5) {
+      moreMsg.classList.add('show');
+    }
+  
+    // If we reached end, reshuffle
+    if (index >= shuffled.length) {
+      resetShuffle();
+    }
+  
+    const id = shuffled[index];
+    index++;
+  
+    rec.innerHTML = `
+      <iframe
+        src="https://open.spotify.com/embed/track/${id}?utm_source=generator"
+        width="100%" height="80" frameborder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+    `;
+  
+    rec.classList.remove('show');
+    void rec.offsetWidth;
+    rec.classList.add('show');
+  });
   
